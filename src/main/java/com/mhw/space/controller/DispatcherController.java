ec16774b7a9860a272dabebc906523e8fb02ee23
maohaiwen <maohaiwen@10.12.68.94>
2017-09-29 14:03:51 +0800

    aaa

--- src/main/java/com/mhw/space/controller/DispatcherController.java
+++ src/main/java/com/mhw/space/controller/DispatcherController.java
@@ -1,5 +1,7 @@
 package com.mhw.space.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +12,8 @@ import com.mhw.space.util.common.DispatcherConstants;
 public class DispatcherController {
 
 	@RequestMapping(value="homePage")
-	public String homePage() {
+	public String homePage(HttpServletResponse response) {
+		response.addHeader("Access-Control-Allow-Origin","*");
 		return DispatcherConstants.HOME_PAGE;
 	}
 }
